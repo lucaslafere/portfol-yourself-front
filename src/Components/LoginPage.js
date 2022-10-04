275bca3b4b83d5df2672bd5b351bbb3742f7a503
Lucas Lafèré <laferelucas@gmail.com>
2022-10-04 14:59:40 -0300

    feat: added react loader spinner

---
+++ src/Components/LoginPage.js
@@ -0,0 +1,5 @@
+import { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+import { ThreeDots } from 'react-loader-spinner';
