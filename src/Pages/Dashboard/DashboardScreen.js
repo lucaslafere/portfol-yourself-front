ac7c4e2e4ecfe4a6b5b8473f55a35d28cfdd15a5
Lucas Lafèré <laferelucas@gmail.com>
2022-10-09 20:33:33 -0300

    Update DashboardScreen.js

--- src/Pages/Dashboard/DashboardScreen.js
+++ src/Pages/Dashboard/DashboardScreen.js
@@ -227,7 +227,7 @@ export default function DashboardScreen() {
       return (
         <S.Container layout={layout.style}>
           <S.TopSection>
-            <S.Title>{layout.title}</S.Title>
+            <S.Title layout={layout.style}>{layout.title}</S.Title>
           </S.TopSection>
           <S.Content>{renderItems}</S.Content>
           <S.Footer layout={layout.style}>
